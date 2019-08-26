b78fdd2b75469765d5a65ff1bac2df2bc56974df
王友全 <798725792@qq.com>
2019-08-26 16:45:49 +0800

    www

--- routes/admin/product.js
+++ routes/admin/product.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const DB = require('../../model/mysqlDB');
 const tools = require('../../model/tools')
 router.post('/list', async (req, res, next) => {
-  console.log(req.body)
   let {page, limit} = req.body
   let sqlTatol = 'select * from product';
   let total = await DB.query(sqlTatol)
@@ -95,20 +94,19 @@ router.post('/edit', async (req, res, next) => {
   istop = istop?1:0;
   status = status?1:0;
   recommend = recommend?1:0;
-  let sql = 'update product set title=?, pic=?, groupImg=?,spec=?, price=?, recommend=?, istop=?, status=?, time=?, content=?, catid=? where id=?';
-  let sql = 'insert into product (title,pic,recommend,istop,status,time,author,visits,content,catid,attr,groupImg) values (?,?,?,?,?,?,?,?,?,?,?,?)';
-    const result = await DB.query(sql, [title,pic,recommend,istop,status,time,author,visits,content,catid,attr,groupImg]);
+  let sql = 'update product set title=?,pic=?,recommend=?,istop=?,status=?,time=?,content=?,catid=?,attr=?,groupImg=?';
+    const result = await DB.query(sql, [title,pic,recommend,istop,status,time,content,catid,attr,groupImg,id]);
     if(result){
       res.json({
         status: 1,
         data: [],
-        msg: '添加成功'
+        msg: '编辑成功'
       })
     } else {
       res.json({
         status: 0,
         data: [],
-        msg: '添加失败'
+        msg: '编辑失败'
       })
     }
 })
